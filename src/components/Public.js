e8e59913fb0f110d8c27e53d26ba3dff876c629d
milosmekota <milos.mekota@post.cz>
2024-11-27 23:17:12 +0100

    frontend refactoring

--- src/components/Public.js
+++ src/components/Public.js
@@ -15,7 +15,7 @@ const Public = () => {
           Located in Beautiful Downtown Foo City, Dan D. Repairs provides a
           trained staff ready to meet your tech repair needs.
         </p>
-        <adress className="public__addr">
+        <address className="public__addr">
           Dan D. Repairs
           <br />
           555 Foo Drive
@@ -23,7 +23,7 @@ const Public = () => {
           Foo City, CA 12345
           <br />
           <a href="tel:+15555555555">(555) 555-5555</a>
-        </adress>
+        </address>
         <br />
         <p>Owner: Dan Davidson</p>
       </main>

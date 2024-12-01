7b824d3cdaa19052c4f020c160f7237550a0e0d1
milosmekota <milos.mekota@post.cz>
2024-12-01 16:28:09 +0100

    new base url

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://technotes-api.onrender.com",
+  baseUrl: "https://technotes-api-s6py.onrender.com/",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
@@ -15,9 +15,9 @@ const baseQuery = fetchBaseQuery({
 });
 
 const baseQueryWithReauth = async (args, api, extraOptions) => {
-  // console.log(args);
-  // console.log(api);
-  // console.log(extraOptions);
+  console.log(args);
+  console.log(api);
+  console.log(extraOptions);
 
   let result = await baseQuery(args, api, extraOptions);
 
